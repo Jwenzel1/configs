d88714b2212c4b7b914ed8b5cb29abb9518a5f40
Joe <10747934+Jwenzel1@users.noreply.github.com>
2025-09-02 14:47:22 -0400

    Give special consideration for docker

--- dotfiles/make-links.sh
+++ dotfiles/make-links.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ls -d */ | awk -F / '{print $1}' | xargs stow --adopt --dotfiles --no-folding --verbose=1 --target=$HOME
+ls -d */ | awk -F / '{print $1}' | xargs stow --adopt --dotfiles --no-folding --target=$HOME --verbose=1
+
+# Docker does not like having a symlink as its config file
+rm -f $HOME/.docker/daemon.json
+cp docker/dot-docker/daemon.json $HOME/.docker/daemon.json
